b3c226fe27456b6c395c0f914a1f149e7395921c
Yu <jimyu0414@gmail.com>
2019-11-12 13:14:40 +1100

    setup styling and bootstrap modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Main.scss';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+import BrocoModal from './components/BrocoModal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    showModal : false,
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App__header">
+            <div className="container">
+              <h1>Broccoli & Co</h1>
+            </div>
+        </header>
+        <div className="App__request">
+        <div className="container">
+          <h1>A better way <br/>to enjoy every day.</h1>
+          <p>Be the first to know hwen we launch.</p>
+          <ButtonToolbar>
+            <Button 
+            className="button button__simple"
+            onClick={() => this.setState({
+              showModal : true
+            })}
+            >
+              Request an invite</Button>
+          </ButtonToolbar>
+          <BrocoModal 
+          show={this.state.showModal}
+          onHide={() => this.setState(
+            {showModal : false}
+          )}
+        />
+        </div>
+        </div>
+        <footer className="App__footer">
+          <div className="container">
+                <p>Broccoli is an edible green plant in the cabbage family
+                  <br />
+                  whose large flowering head and stalk is eaten as a vegetable.
+                </p>
+            </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BrocoModal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import BrocoForm from '../components/BrocoForm';
+
+class BrocoModal extends React.Component{
+
+    render(){
+       return(
+        <Modal
+        show={this.props.show}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+
+      >
+        <Modal.Header >
+          <Modal.Title id="contained-modal-title-vcenter">
+            Request an invite
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+            
+       );
+    }
+}
+
+export default BrocoModal;
